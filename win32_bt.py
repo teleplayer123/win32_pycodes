ebe88d56b8e049c9525b72e4d63f645e738630fd
cole sashkin <csashceh1024@gmail.com>
2024-07-16 15:42:39 -0700

    first commit

---
+++ win32_bt.py
@@ -0,0 +1,153 @@
+from ctypes import *
+from ctypes.wintypes import DWORD, HANDLE, BOOL, CHAR, BYTE, WCHAR, ULONG, USHORT, WORD
+
+
+btapi = windll.LoadLibrary("BluetoothApis.dll")
+
+BTH_ADDR = c_ulonglong
+BTADDR_BYTE = c_ubyte * 6
+
+BLUETOOTH_MAX_NAME_SIZE = 248
+
+ERROR_SUCCESS = 0
+
+
+BLUETOOTH_AUTHENTICATION_METHOD = {
+    0x00: "bluetooth_authentication_method_legacy",
+    0x01: "bluetooth_authentication_method_oob",
+    0x02: "bluetooth_authentication_method_numeric_comparison",
+    0x03: "bluetooth_authentication_method_passkey_notification",
+    0x04: "bluetooth_authentication_method_passkey"
+}
+
+class SYSTEMTIME(Structure):
+    _fields_ = [
+        ("wYear", WORD),
+        ("wMonth", WORD),
+        ("wDayOfWeek", WORD),
+        ("wDay", WORD),
+        ("wHour", WORD),
+        ("wMinute", WORD),
+        ("wSecond", WORD),
+        ("wMilliseconds", WORD)
+    ]
+
+class BLUETOOTH_ADDRESS(Union):
+    _fields_ = [
+        ("ullLong", BTH_ADDR),
+        ("rgBytes", BTADDR_BYTE)
+    ]
+
+class BLUETOOTH_DEVICE_INFO(Structure):
+    _fields_ = [
+        ("dwSize", DWORD),
+        ("address", BLUETOOTH_ADDRESS),
+        ("ulClassofDevice", ULONG),
+        ("fConnected", BOOL),
+        ("fRemembered", BOOL),
+        ("fAuthenticated", BOOL),
+
+    ]
+
+class BLUETOOTH_FIND_RADIO_PARAMS(Structure):
+    _fields_ = [
+        ("dwSize", DWORD)
+    ]
+
+class BLUETOOTH_DEVICE_SEARCH_PARAMS(Structure):
+    _fields_ = [
+        ("dwSize", DWORD),
+        ("fReturnAuthenticated", BOOL),
+        ("fReturnRemembered", BOOL),
+        ("fReturnUnknown", BOOL),
+        ("fReturnConnected", BOOL),
+        ("fIssueInquiry", BOOL),
+        ("cTimeoutMultiplier", CHAR),
+        ("hRadio", HANDLE)
+    ]
+
+class BLUETOOTH_RADIO_INFO(Structure):
+    _fields_ = [
+        ("dwSize", DWORD),
+        ("address", BLUETOOTH_ADDRESS),
+        ("szName", WCHAR * BLUETOOTH_MAX_NAME_SIZE),
+        ("ulClassofDevice", ULONG),
+        ("lmpSubversion", USHORT),
+        ("manufacturer", USHORT)
+    ]
+
+def BluetoothGetRadioInfo(h_find):
+    func_ref = btapi.BluetoothGetRadioInfo
+    func_ref.argtypes = [HANDLE, POINTER(BLUETOOTH_RADIO_INFO)]
+    func_ref.restype = DWORD
+    btri = BLUETOOTH_RADIO_INFO()
+    dres = func_ref(h_find, byref(btri))
+    print(f"BluetoothGetRadioInfo Result: {dres}")
+    return btri
+
+
+def BluetoothGetDeviceInfo(h_dev):
+    func_ref = btapi.BluetoothGetDeviceInfo
+    func_ref.argtypes = [HANDLE, POINTER(BLUETOOTH_DEVICE_INFO)]
+    func_ref.restype = DWORD
+    btdi = BLUETOOTH_DEVICE_INFO()
+    dres = func_ref(h_dev, byref(btdi))
+    return btdi
+
+
+def BluetoothFindFirstRadio():
+    """
+        HBLUETOOTH_RADIO_FIND BluetoothFindFirstRadio(
+            const BLUETOOTH_FIND_RADIO_PARAMS *pbtfrp,
+            [out] HANDLE                      *phRadio
+        );
+    """
+    func_ref = btapi.BluetoothFindFirstRadio
+    func_ref.argtypes = [POINTER(BLUETOOTH_FIND_RADIO_PARAMS), POINTER(HANDLE)]
+    func_ref.restype = DWORD
+    ph_radio = HANDLE()
+    btfrp = BLUETOOTH_FIND_RADIO_PARAMS()
+    h_find = func_ref(byref(btfrp), byref(ph_radio))
+    return h_find, ph_radio
+
+
+def BluetoothFindNextRadio(h_find):
+    """
+        BOOL BluetoothFindNextRadio(
+            [in]  HBLUETOOTH_RADIO_FIND hFind,
+            [out] HANDLE                *phRadio
+        );
+    """
+    func_ref = btapi.BluetoothFindNextRadio
+    func_ref.argtypes = [HANDLE, POINTER(HANDLE)]
+    func_ref.restype = BOOL
+    ph_radio = HANDLE()
+    bres = func_ref(h_find, byref(ph_radio))
+    print(f"Bool: {bres}")
+    return ph_radio
+
+
+def BluetoothFindFirstDevice(h_find):
+    func_ref = btapi.BluetoothFindFirstDevice
+    func_ref.argtypes = [POINTER(BLUETOOTH_DEVICE_SEARCH_PARAMS), POINTER(BLUETOOTH_DEVICE_INFO)]
+    func_ref.restype = HANDLE
+    btdsp = BLUETOOTH_DEVICE_SEARCH_PARAMS()
+    btdsp.dwSize = sizeof(btdsp)
+    btdi = BLUETOOTH_DEVICE_INFO()
+    h_dev = func_ref(byref(btdsp), byref(btdi))
+    return h_dev
+
+
+
+def main():
+    h_find, ph_radio = BluetoothFindFirstRadio()
+    print(f"hFind Handle: {h_find}")
+    print(f"phRadio Handle: {ph_radio}")
+    r_info = BluetoothGetRadioInfo(h_find)
+    print(f"RadioInfo Address: {r_info.address.rgBytes[:]}")
+    
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ windows_wlan.py
@@ -0,0 +1,130 @@
+import ctypes as ct
+from ctypes.wintypes import DWORD, HANDLE, WORD, BOOLEAN, BYTE, WCHAR, UINT
+from enum import Enum
+
+ERROR_SUCCESS = 0
+
+class GUID(ct.Structure):
+    _fields_ = [
+        ("Data1", DWORD),
+        ("Data2", WORD),
+        ("Data3", WORD),
+        ("Data4", BYTE * 8)
+    ]
+
+class WLAN_CONNECTION_MODE(Enum):
+    wlan_connection_mode_profile = 0,
+    wlan_connection_mode_temporary_profile = 1
+    wlan_connection_mode_discovery_secure = 2
+    wlan_connection_mode_discovery_unsecure = 3
+    wlan_connection_mode_auto = 4
+    wlan_connection_mode_invalid = 5
+
+class WLAN_INTF_OPCODE(Enum):
+    wlan_intf_opcode_autoconf_start = 0
+    wlan_intf_opcode_autoconf_enabled = 1
+    wlan_intf_opcode_background_scan_enabled = 2
+    wlan_intf_opcode_media_streaming_mode = 3
+    wlan_intf_opcode_radio_state = 4
+    wlan_intf_opcode_bss_type = 5
+    wlan_intf_opcode_interface_state = 6
+    wlan_intf_opcode_current_connection = 7
+    wlan_intf_opcode_channel_number = 8
+    wlan_intf_opcode_supported_infrastructure_auth_cipher_pairs = 9
+    wlan_intf_opcode_supported_adhoc_auth_cipher_pairs = 10
+    wlan_intf_opcode_supported_country_or_region_string_list = 11
+    wlan_intf_opcode_current_operation_mode = 12
+    wlan_intf_opcode_supported_safe_mode = 13
+    wlan_intf_opcode_certified_safe_mode = 14
+    wlan_intf_opcode_hosted_network_capable = 15
+    wlan_intf_opcode_management_frame_protection_capable = 16
+    wlan_intf_opcode_secondary_sta_interfaces = 17
+    wlan_intf_opcode_secondary_sta_synchronized_connections = 18
+    wlan_intf_opcode_autoconf_end = 0x0fffffff
+    wlan_intf_opcode_msm_start = 0x10000100
+    wlan_intf_opcode_statistics = 0x10000101
+    wlan_intf_opcode_rssi = 0x10000102
+    wlan_intf_opcode_msm_end = 0x1fffffff
+    wlan_intf_opcode_security_start = 0x20010000
+    wlan_intf_opcode_security_end = 0x2fffffff
+    wlan_intf_opcode_ihv_start = 0x30000000
+    wlan_intf_opcode_ihv_end = 0x3fffffff
+
+WLAN_INTERFACE_STATE_T = UINT
+class WLAN_INTERFACE_STATE(Enum):
+    wlan_interface_state_not_ready = 0
+    wlan_interface_state_connected = 1
+    wlan_interface_state_ad_hoc_network_formed = 2
+    wlan_interface_state_disconnecting = 3
+    wlan_interface_state_disconnected = 4
+    wlan_interface_state_associating = 5
+    wlan_interface_state_discovering = 6
+    wlan_interface_state_authenticating = 7
+
+
+class WLAN_INTERFACE_INFO(ct.Structure):
+    _fields_ = [
+        ("InterfaceGuid", GUID),
+        ("strInterfaceDescription", WCHAR),
+        ("isState", WLAN_INTERFACE_STATE_T)
+    ]
+
+class WLAN_INTERFACE_INFO_LIST(ct.Structure):
+    _fields_ = [
+        ("dwNumberOfItems", DWORD),
+        ("dwIndex", DWORD),
+        ("InterfaceInfo", WLAN_INTERFACE_INFO * 1)
+    ]
+
+
+class Win32_WlanApi:
+
+    def __init__(self):
+        self._handle = self.WlanOpenHandle()
+        self._wlanapi = ct.windll.LoadLibrary("wlanapi.dll")
+
+    def WlanOpenHandle(self):
+        """
+        DWORD WlanOpenHandle(
+        [In] DWORD dwClientVersion,
+        [Reserved] PVOID pReserved,
+        [Out] PWORD pdwNegotiatedVersion
+        [Out] PHANDLE phClientHandle
+        )
+        """
+        func_ref = self._wlanapi.WlanOpenHandle
+        func_ref.argtypes = [DWORD, ct.c_void_p, ct.POINTER(DWORD), ct.POINTER(HANDLE)]  #func argument types 
+        func_ref.restype = DWORD  #func return type
+        negotiated_ver = DWORD()  #dword holder for pdwNegotiatedVersion reference
+        client_handle = HANDLE()  #handle holder for client handle reference
+        client_ver = 2  #predefined value client version for newer windows versions
+        p_reserved = None  #void variable for reserved param
+        res = func_ref(client_ver, p_reserved, ct.byref(negotiated_ver), ct.byref(client_handle))  #byref() used for [Out] params that will be assigned value
+        if res != ERROR_SUCCESS:
+            raise Exception("Error trying to open wlan handle")
+        return client_handle
+
+    def WlanCloseHandle(self):
+        """
+        DWORD WlanCloseHandle(
+        [In] HANDLE pClientHandle,
+        [Reserved] PVOID pReserved
+        )
+        """
+        func_ref = self._wlanapi.WlanCloseHandle
+        func_ref.argtypes = [HANDLE, ct.c_void_p]
+        func_ref.restype = DWORD
+        res = func_ref(self._handle, None)
+        if res != ERROR_SUCCESS:
+            raise Exception("Error closing wlan handle")
+        return res
+
+    def WlanEnumInterfaces(self):
+        func_ref = self._wlanapi.WlanEnumInterfaces
+        func_ref.argtypes = [HANDLE, ct.c_void_p, ct.POINTER(ct.POINTER(WLAN_INTERFACE_INFO_LIST))]
+        func_ref.restype = DWORD
+        intf_list = ct.pointer(WLAN_INTERFACE_INFO_LIST())
+        res = func_ref(self._handle, None, ct.byref(intf_list))
+        if res != ERROR_SUCCESS:
+            raise Exception("Error enumerating interfaces")
+        return res
